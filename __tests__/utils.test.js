9d7fa83869e880fef5fc26ff13d2822fac51efb4
Mahamud Arteh <artehrush@gmail.com>
2021-08-02 14:38:32 +0100

    utils first test block passing

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,12 +1,14 @@
-const formatData = require('../db/utils/data-manipulation.js');
+const { formatData } = require('../db/utils/data-manipulation.js');
 
 describe('formatData()', () => {
     test('Returns new array', () => {
         const input = [{}];
-        expect(formatData(input)).not.ToBe(input);
+        console.log(formatData(input), '<-- output')
+        expect(formatData(input)).not.toBe(input);
         expect(Array.isArray(formatData(input)));
     })
     test('Returns nested array of object values for single object', () => {
+        console.log
         const input = [{ slug: 'euro game', description: 'Abstact games that involve little luck' }]
         expect(formatData(input)).toEqual([['euro game', 'Abstact games that involve little luck']])
     })
@@ -15,7 +17,9 @@ describe('formatData()', () => {
         expect(formatData(input)).toEqual([['euro game', 'Abstact games that involve little luck'], [`children's games`, `Games suitable for children`]])
     })
     test('Returns nested array of object values for objects with more properties', () => {
-        
+        const input = [{ slug: 'euro game', description: 'Abstact games that involve little luck' }, { slug: "children's games", description: 'Games suitable for children' },{ slug: 'euro game', description: 'Abstact games that involve little luck' }];
+
+        expect(formatData(input)).toEqual([['euro game', 'Abstact games that involve little luck'], ["children's games", 'Games suitable for children'], ['euro game', 'Abstact games that involve little luck']]);
     })
 
-})
+});
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,8 +1,15 @@
 // extract any functions you are using to manipulate your data, into this file
 
 const formatData = (arrOfObjs) => {
-    //an array of arrays with obj values
-    
+    const copy = [...arrOfObjs];
+    const result = copy.map((object) => {
+        let newArr = []
+        for (const key in object) {
+            newArr.push(object[key])
+        }
+        return newArr;
+    })
+    return result;
 }
 
 module.exports = { formatData }
