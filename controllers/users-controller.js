7006beadb0d70ed9619c6eb99010ed55f1c0681f
Sgavinmills <sgavinmills@gmail.com>
2021-08-05 11:39:29 +0100

    heroku setup

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -6,6 +6,7 @@ const getUsers = (req, res, next) => {
     selectUsers().then((users) => {
         res.status(200).send({users});
     }).catch(err => {
+        console.log(err);
         next(err);
     })
 }
