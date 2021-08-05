9bcd87a003cd81963f46df3a626f8259749b3fdf
Sgavinmills <sgavinmills@gmail.com>
2021-08-05 11:37:21 +0100

    heroku setup

--- models/users-model.js
+++ models/users-model.js
@@ -3,6 +3,8 @@ const db = require('../db/connection.js');
 
 const selectUsers = async () => {
     const qryResponse = await db.query('SELECT username FROM users');
+    console.log(qryResponse);
+    console.log('users qry response ^^^^^');
     return qryResponse.rows;
 }
 
