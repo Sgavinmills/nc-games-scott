04ec97470f43b9d35540cbe3878a67790381a3f5
Sgavinmills <sgavinmills@gmail.com>
2021-08-27 11:37:36 +0100

    CORS enabled

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { invalidRoute, PSQLerrorHandling, serverErrorHandling, customErrorHandling } = require('./errors/errorHandling');
 const apiRouter = require('./routers/api-router');
+const cors = require('cors');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
 app.use('/api', apiRouter);
 
 
