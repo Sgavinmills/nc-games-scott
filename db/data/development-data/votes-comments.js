44c1d98b426eec9daac960648b3596c26f081958
Sgavinmills <sgavinmills@gmail.com>
2021-08-29 20:35:56 +0100

    updated development data with vote_type info

--- db/data/development-data/votes-comments.js
+++ db/data/development-data/votes-comments.js
@@ -1,42 +1,52 @@
 module.exports = [
     {
         comment_id : 23,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         comment_id : 1,
-        voted_by : "grumpy19"
+        voted_by : "grumpy19",
+        vote_type : 'up'
     },
     {
         comment_id : 1,
-        voted_by : "cooljmessy"
+        voted_by : "cooljmessy",
+        vote_type : 'up'
     },
     {
         comment_id : 2,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         comment_id : 7,
-        voted_by : "weegembump"
+        voted_by : "weegembump",
+        vote_type : 'up'
     },
     {
         comment_id : 4,
-        voted_by : "weegembump"
+        voted_by : "weegembump",
+        vote_type : 'up'
     },
     {
         comment_id : 14,
-        voted_by : "jessjelly"
+        voted_by : "jessjelly",
+        vote_type : 'up'
     },
     {
         comment_id : 24,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         comment_id : 20,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         comment_id : 23,
-        voted_by : "happyamy2016"
+        voted_by : "happyamy2016",
+        vote_type : 'up'
     },
 ]
--- db/data/development-data/votes-reviews.js
+++ db/data/development-data/votes-reviews.js
@@ -1,43 +1,53 @@
 module.exports = [
     {
         review_id : 1,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         review_id : 1,
-        voted_by : "grumpy19"
+        voted_by : "grumpy19",
+        vote_type : 'up'
     },
     {
         review_id : 1,
-        voted_by : "cooljmessy"
+        voted_by : "cooljmessy",
+        vote_type : 'up'
     },
     {
         review_id : 2,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         review_id : 3,
-        voted_by : "weegembump"
+        voted_by : "weegembump",
+        vote_type : 'up'
     },
     {
         review_id : 4,
-        voted_by : "weegembump"
+        voted_by : "weegembump",
+        vote_type : 'up'
     },
     {
         review_id : 18,
-        voted_by : "jessjelly"
+        voted_by : "jessjelly",
+        vote_type : 'up'
     },
     {
         review_id : 19,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         review_id : 20,
-        voted_by : "tickle122"
+        voted_by : "tickle122",
+        vote_type : 'up'
     },
     {
         review_id : 14,
-        voted_by : "happyamy2016"
+        voted_by : "happyamy2016",
+        vote_type : 'up'
     },
 ]
 
